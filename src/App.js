591ef865e06d485dd6d40d9a36092b32ba0820f0
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2022-01-31 17:17:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
 
-function App() {
+export default function App() {
+  const [peso, setPeso] = useState(''); /* setState utiliado para o componente renderizar novamente */
+  const [altura, setAltura] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function calcularIMC() {
+    const alt = altura / 100;
+    const imc = (peso / (alt * alt)).toFixed(2); /* toFixed() para mostrar apenas 2 casas decimais */
+
+    if (imc < 18.6) {
+      setMensagem(`Você está abaixo do peso! Seu IMC: ${imc}`) /* Seta em 'mensagem' essa frase */
+    } else if (imc < 24.9) {
+      setMensagem(`Peso ideal! Seu IMC: ${imc}`)
+    } else if (imc < 34.9) {
+      setMensagem(`Você está levemente acima do peso! Seu IMC: ${imc}`)
+    } else if (imc >= 35) {
+      setMensagem(`Cuidado! Obesidade! Seu IMC: ${imc}`)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC</span>
 
-export default App;
+      <div className="area-input">
+        <input 
+          type="text"
+          placeholder="Peso em (kg) Ex: 90"
+          value={peso}
+          onChange={ (e) => setPeso(e.target.value)} /* Acessa o que o usuário está digitando e muda o valor */
+        />
+        <input 
+          type="text"
+          placeholder="Altura em (cm) Ex: 180"
+          value={altura}
+          onChange={ (e) => setAltura(e.target.value)}
+        />
+
+        <button onClick={calcularIMC}> {/* Ao clicar, chama a função que irá calcular */}
+          Calcular
+        </button>
+      </div>
+
+      <h2>{mensagem}</h2> {/* Irá mostrar em tela o que tem em 'mensagem' */}
+    </div>
+  )
+}
